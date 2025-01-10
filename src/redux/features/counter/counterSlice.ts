fc6569dbc4e056355dc269e42cd9512c71817b85
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2025-01-10 14:25:43 +0600

    project created

---
+++ src/redux/features/counter/counterSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state, action: PayloadAction<number>) => {
+      state.count = state.count + action.payload;
+    },
+    decrement: (state) => {
+      state.count = state.count - 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export default counterSlice.reducer;
